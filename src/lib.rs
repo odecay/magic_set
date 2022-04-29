6ee9a16800fc8c260632b7f9a620de0e3a15247b
otis <electricbuck@gmail.com>
2022-04-29 02:52:42 -0400

    added match checking, test visual selection removal

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ use rand::{
     thread_rng, Rng,
 };
 
-// pub mod tiles?
 mod helpers;
 
 pub struct MagicSetPlugin;
@@ -23,6 +22,8 @@ impl Plugin for MagicSetPlugin {
             .register_inspectable::<Color>()
             .register_inspectable::<Shape>()
             .add_system(set_mark)
+            .add_system(check_match)
+            .add_system(remove_mark)
             .add_system(helpers::set_texture_filters_to_nearest);
 
         //.add_system(build_map);
@@ -39,7 +40,7 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query
     let map_entity = commands.spawn().id();
     let mut map = Map::new(0u16, map_entity);
     let layer_settings = LayerSettings::new(
-        MapSize(4, 3),
+        MapSize(10, 4),
         ChunkSize(2, 2),
         TileSize(32.0, 32.0),
         TextureSize(96.0, 96.0),
@@ -54,6 +55,7 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query
 
     //layer_builder.set_all(TileBundle::default());
 
+    //current random board generation/initialization, adds random color and shape components
     layer_builder.for_each_tiles_mut(|tile_entity, tile_data| {
         let random_color: Color = rand::random();
         let random_shape: Shape = rand::random();
@@ -66,7 +68,6 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query
             TilePos::default(),
         ));
 
-        //figure out how to randomize selecting an enum variant
         if tile_entity.is_none() {
             *tile_entity = Some(commands.spawn().id());
         }
@@ -78,7 +79,6 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query
 
     map_query.build_layer(&mut commands, layer_builder, texture_handle);
 
-    // commands.entity(layer_entity).insert(LastUpdate::default());
     commands.entity(layer_entity);
 
     map.add_layer(&mut commands, 0u16, layer_entity);
@@ -92,33 +92,13 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>, mut map_query
     commands.insert_resource(layer_settings);
     commands.insert_resource(center_res);
 }
-//rename to build_board or generate board?
-//how to add custom components for each tile?
+
 //ok we need a better way than random samples to build the board tiles (maybe)
+//waveform collapse?
+//psudeorandom algorithm of some kind
 
-// fn build_map(mut map_query: MapQuery, mut commands: Commands) {
-//     let mut seed = thread_rng();
-
-//     //might need diff range for loop
-//     for _ in 0..2 {
-//         let position = TilePos(seed.gen_range(0..16), seed.gen_range(0..12));
-
-//         let _ = map_query.set_tile(
-//             &mut commands,
-//             position,
-//             Tile {
-//                 texture_index: seed.gen_range(0..10),
-//                 ..Default::default()
-//             },
-//             0u16,
-//             0u16,
-//         );
-//         map_query.notify_chunk_for_tile(position, 0u16, 0u16);
-//     }
-// }
-
-fn set_tiles(mut query: Query<(&Color, &Shape, &mut Tile, &TilePos)>) {
-    for (color, shape, mut tile, tile_pos) in query.iter_mut() {
+fn set_tiles(mut query: Query<(&Color, &Shape, &mut Tile)>) {
+    for (color, shape, mut tile) in query.iter_mut() {
         let c = match color {
             Color::Blue => 0,
             Color::Red => 1,
@@ -131,15 +111,11 @@ fn set_tiles(mut query: Query<(&Color, &Shape, &mut Tile, &TilePos)>) {
         };
         let index = c + s;
         tile.texture_index = index;
-        // println!(
-        //     "texture_index {:?}, tilepos {:?}",
-        //     tile.texture_index, tile_pos
-        // );
     }
 }
 
 fn update_tiles(
-    mut commands: Commands,
+    // mut commands: Commands,
     mut map_query: MapQuery,
     mut query: Query<(&mut Color, &mut Shape, &TilePos)>,
 ) {
@@ -154,10 +130,13 @@ fn set_mark(
     mut query: Query<&TilePos, With<Cursor>>,
     mut map_query: MapQuery,
     mut commands: Commands,
+    keys: Res<Input<KeyCode>>,
 ) {
-    for position in query.iter_mut() {
-        let tile_entity = map_query.get_tile_entity(*position, 0u16, 0u16).unwrap();
-        commands.entity(tile_entity).insert(Mark);
+    if keys.just_pressed(KeyCode::Space) {
+        for position in query.iter_mut() {
+            let tile_entity = map_query.get_tile_entity(*position, 0u16, 0u16).unwrap();
+            commands.entity(tile_entity).insert(Mark);
+        }
     }
 }
 
@@ -168,18 +147,82 @@ fn draw_mark(
     center: Res<Center>,
     query: Query<(&Mark, &TilePos), Added<Mark>>,
 ) {
+    //maybe have this draw as a child of the tile?? how do i remove the visual when i clean up mark
     let handle: Handle<Image> = asset_server.load("select.png");
     let tile_size = layer_settings.tile_size;
     for (_mark, tile_pos) in query.iter() {
-        commands.spawn_bundle(SpriteBundle {
-            texture: handle.clone(),
-            transform: Transform::from_xyz(
-                -center.0.x + (tile_size.0) * tile_pos.0 as f32 + (tile_size.0 / 2.0),
-                -center.0.y + (tile_size.1) * tile_pos.1 as f32 + (tile_size.1 / 2.0),
-                0.4,
-            ),
-            ..Default::default()
-        });
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: handle.clone(),
+                transform: Transform::from_xyz(
+                    -center.0.x + (tile_size.0) * tile_pos.0 as f32 + (tile_size.0 / 2.0),
+                    -center.0.y + (tile_size.1) * tile_pos.1 as f32 + (tile_size.1 / 2.0),
+                    0.4,
+                ),
+                ..Default::default()
+            })
+            .insert(Selection);
+        // commands.entity(entity).push_children(&[child]);
+    }
+}
+fn remove_mark(
+    mut commands: Commands,
+    query: Query<Entity, With<Mark>>,
+    sprite_query: Query<Entity, With<Selection>>,
+) {
+    if query.iter().count() == 3 {
+        println!("3+ marks");
+        for entity in query.iter() {
+            commands.entity(entity).remove::<Mark>();
+        }
+    }
+
+    //probably move this into its own system
+    if sprite_query.iter().count() == 3 {
+        for entity in sprite_query.iter() {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn check_match(query: Query<(&Color, &Shape), With<Mark>>) {
+    let (mut colors, mut shapes): (Vec<_>, Vec<_>) = query.iter().unzip();
+    let mut color_match = false;
+    if colors.len() == 3 {
+        colors.sort_unstable();
+        // println!("{:?}", colors);
+        let mut unique_colors = colors.clone();
+        unique_colors.dedup();
+        println!("{:?}", unique_colors);
+        if colors.len() == unique_colors.len() {
+            color_match = true;
+            // println!("match of different colors!")
+        } else if unique_colors.len() == 1 {
+            color_match = true;
+            // println!("match of same color!")
+        } else {
+            // println!("no match")
+        }
+    }
+    let mut shape_match = false;
+    if shapes.len() == 3 {
+        shapes.sort_unstable();
+        // println!("{:?}", shapes);
+        let mut unique_shapes = shapes.clone();
+        unique_shapes.dedup();
+        println!("{:?}", unique_shapes);
+        if shapes.len() == unique_shapes.len() {
+            shape_match = true;
+            // println!("match of different shapes!")
+        } else if unique_shapes.len() == 1 {
+            shape_match = true;
+            // println!("match of same shape!")
+        } else {
+            // println!("no match")
+        }
+    }
+    if shape_match == true && color_match == true {
+        println!("Match!!")
     }
 }
 
@@ -190,12 +233,9 @@ fn spawn_cursor(
 ) {
     let handle: Handle<Image> = asset_server.load("cursor.png");
     for layer in query.get_single() {
-        println!("found layersettings, spawning cursor");
         let tile_pos = TilePos(0, 0);
         let center = layer.settings.get_pixel_center();
         let tile_size = layer.settings.tile_size;
-        // let center = Vec2::new(x = 128.0, y = 128.0);
-        // let center = (128.0, 128.0);
         commands
             .spawn_bundle(SpriteBundle {
                 texture: handle.clone(),
@@ -209,37 +249,19 @@ fn spawn_cursor(
             .insert(Cursor)
             .insert(tile_pos);
     }
-    // let tile_size = &layer.settings.tile_size;
-
-    // let tile_size = TileSize(32.0, 32.0);
-    // let tile_pos = TilePos(0, 0);
-    // x_offset = ((map_size.0 * chunk_size.0) as f32 * tile_size.0) / 2.0
-    // y_offset = ((map_size.1 * chunk_size.1) as f32 * tile_size.1) / 2.0
-    // ((self.map_size.0 * self.chunk_size.0) as f32 * self.tile_size.0) / 2.0,
-    // ((self.map_size.1 * self.chunk_size.1) as f32 * self.tile_size.1) / 2.0,
-    // let x = tile_size.0 * tile_pos.0 as f32 + tile_size.0 / 2.0;
-    // let y = tile_size.1 * tile_pos.1 as f32 + tile_size.1 / 2.0;
-
-    // let translation = Vec3::new(x, y, 1.0);
-    // commands
-    //     .spawn_bundle(SpriteBundle {
-    //         texture: handle,
-    //         transform: Transform::from_xyz(-center.x, -center.y, 1.0),
-    //         ..Default::default()
-    //     })
-    //     .insert(Cursor)
-    //     .insert(tile_pos);
-    // let cursor = commands.spawn().insert(Cursor).insert(tile_pos);
 }
 
 fn move_cursor(
     mut query: Query<(&mut TilePos, &mut Transform), With<Cursor>>,
+    layer_settings: Res<LayerSettings>,
     keys: Res<Input<KeyCode>>,
 ) {
-    // the oob checks should be changed to check the LayerSettings
+    let map_size: Vec2 = layer_settings.map_size.into();
+    let chunk_size: Vec2 = layer_settings.chunk_size.into();
+    let bounds = map_size * chunk_size;
     for (mut tile_pos, mut transform) in query.iter_mut() {
         if keys.just_pressed(KeyCode::W) {
-            if tile_pos.1 < 5 {
+            if tile_pos.1 < bounds.y as u32 - 1 {
                 *tile_pos = TilePos(tile_pos.0, tile_pos.1 + 1);
                 transform.translation.y += 32.0;
             }
@@ -251,7 +273,7 @@ fn move_cursor(
             }
         }
         if keys.just_pressed(KeyCode::D) {
-            if tile_pos.0 < 7 {
+            if tile_pos.0 < bounds.x as u32 - 1 {
                 *tile_pos = TilePos(tile_pos.0 + 1, tile_pos.1);
                 transform.translation.x += 32.0;
             }
@@ -267,27 +289,24 @@ fn move_cursor(
 
 #[derive(Component)]
 struct Cursor;
-//maybe turn this into a resource? which would just contain a tile position
 
 #[derive(Component)]
 struct Mark;
 
 #[derive(Component)]
-struct Card;
+struct Selection;
 
 #[derive(Component)]
-struct Grid;
+struct Card;
 
-#[derive(Inspectable, Component)]
-// #[derive(Component)]
+#[derive(Inspectable, Component, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Color {
     Blue,
     Red,
     Yellow,
 }
 
-#[derive(Inspectable, Component)]
-// #[derive(Component)]
+#[derive(Inspectable, Component, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Shape {
     Diamond,
     Circle,
@@ -315,24 +334,3 @@ impl Distribution<Shape> for Standard {
         }
     }
 }
-
-// trait SampleableEnum {
-//     fn enum_sample(R: Range(0..=2)) -> Self;
-// }
-
-// impl SampleableEnum for Color {
-//     fn enum_sample(&self) -> Color {
-//         match rng.gen_range(0..=2) {
-//             0 => Color::Red,
-//             1 => Color::Yellow,
-//             _ => Color::Blue,
-
-//         }
-// }
-//     }
-
-// impl Distribution<T: SampleableEnum> for Standard {
-//     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> T {
-//         self.enum_sample()
-//     }
-// }
